47090af5bfb8af0b132a9fc3101d655af6d64ff9
Trishon Baidaya Shontu <106867008+TrishonBaidaya7399@users.noreply.github.com>
2023-08-31 00:40:13 +0530

    Update phone.js

--- JS/phone.js
+++ JS/phone.js
@@ -69,6 +69,7 @@ const handleSearch = (isShowAll) =>{
     if(searchText){
         loadPhone(searchText, isShowAll);
         toggleLoadingSpinner(true);
+        searchField.value= '';
     }
 
 }
